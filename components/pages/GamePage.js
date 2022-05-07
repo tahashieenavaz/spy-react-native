b17819466761214c4c761e4cef87740f9e0e9590
Taha Shieenavaz <tahashieenavaz@gmail.com>
2022-05-07 18:56:49 +0430

    Associated comment added

--- components/pages/GamePage.js
+++ components/pages/GamePage.js
@@ -5,6 +5,12 @@ import words from '../../words.json';
 export default function SettingsPage({ route }) {
   const { spyCount, playerCount } = route.params;
   const theWord = words[~~(Math.random() * words.length)];
+  /**
+   * False represents a normal player who knows what theWord is, and a true
+   * represents an spy.
+   * I used Javascript's native sort function to randomize naively generated array and encapsulated
+   * the logic within a function called randomizeArray in helpers.js file.
+   */
   const playerStack = randomizeArray([
     ...falseArray(playerCount),
     ...trueArray(spyCount),
