75198b15f1d5fa800a009ce0ef60df7cad64a15d
Taha Shieenavaz <tahashieenavaz@gmail.com>
2022-05-07 18:12:58 +0430

    Navigation animation customized

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Text, View } from "react-native";
-import Homepage from "./components/pages/Homepage";
-import SettingsPage from "./components/pages/SettingsPage";
-import GamePage from "./components/pages/GamePage";
+import { NavigationContainer } from '@react-navigation/native';
+import Homepage from './components/pages/Homepage';
+import SettingsPage from './components/pages/SettingsPage';
+import GamePage from './components/pages/GamePage';
+import { Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Homepage" component={Homepage} />
-        <Stack.Screen name="Settings" component={SettingsPage} />
-        <Stack.Screen name="Game" component={GamePage} />
+        <Stack.Group>
+          <Stack.Screen
+            name="Homepage"
+            component={Homepage}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="Settings" component={SettingsPage} />
+          <Stack.Screen name="Game" component={GamePage} />
+        </Stack.Group>
+
+        <Stack.Group options={{ presenation: 'modal' }}></Stack.Group>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- components/pages/Homepage.js
+++ components/pages/Homepage.js
@@ -1,22 +1,22 @@
-import { StatusBar } from "expo-status-bar";
-import Button from "../Button";
-import Logo from "../Logo";
-import { StyleSheet, Text, View, Alert } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import Button from '../Button';
+import Logo from '../Logo';
+import { StyleSheet, Text, View, Alert } from 'react-native';
 
 export default function Homepage({ navigation }) {
   return (
     <View style={styles.container}>
       {/* Below view is added to force push next flex element to center of stack */}
       <View></View>
-      <View style={{ alignItems: "center" }}>
+      <View style={{ alignItems: 'center' }}>
         <Logo />
-        <Text style={{ color: "#fff", fontWeight: "bold", fontSize: 70 }}>
+        <Text style={{ color: '#fff', fontWeight: 'bold', fontSize: 70 }}>
           S.P.Y
         </Text>
         <Text
           style={{
-            color: "#fff",
-            fontWeight: "100",
+            color: '#fff',
+            fontWeight: '100',
             fontSize: 27,
             opacity: 0.5,
           }}
@@ -26,7 +26,7 @@ export default function Homepage({ navigation }) {
         <Button
           style={{ marginTop: 40 }}
           onPress={() =>
-            navigation.navigate("Settings", {
+            navigation.navigate('Settings', {
               randomNumber: ~~(200 * Math.random()),
             })
           }
@@ -34,7 +34,7 @@ export default function Homepage({ navigation }) {
         />
       </View>
       <Text
-        style={{ justifyContent: "flex-end", marginBottom: 30, color: "#fff" }}
+        style={{ justifyContent: 'flex-end', marginBottom: 30, color: '#fff' }}
       >
         Developed By Taha Shieenavaz
       </Text>
@@ -46,8 +46,8 @@ export default function Homepage({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#00297A",
-    alignItems: "center",
-    justifyContent: "space-between",
+    backgroundColor: '#00297A',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
 });
